9e0ea4668f27c45da55a44bb4e96c2b0714cd11f
LeoGotardo <leonardo.gotardo2@gmail.com>
2024-10-10 16:20:01 -0300

    fixing issues

--- ATM/caixa_eletronico.c
+++ ATM/caixa_eletronico.c
@@ -12,7 +12,7 @@ int nota(nota, valor)
       vez += 1;
     }
 
-  return valor;
+  return valor, vez;
 }
 
 int caixa()
@@ -22,12 +22,13 @@ int caixa()
   printf("Qual o valor que deseja sacar?\n");
   scanf("%d",&valor);
 
-  nota100 = nota(100);
-  nota50 = nota(50);
-  nota20 = nota(20);
-  nota10 = nota(10);
-  nota5 = nota(5);
-  nota1 = nota(1);
+  valor, nota100 = nota(100, valor);
+
+  valor, nota50 = nota(50, valor);
+  valor ,nota20 = nota(20, valor);
+  valor, nota10 = nota(10, valor);
+  valor, nota5 = nota(5, valor);
+  valor, nota1 = nota(1, valor);
 
   printf("Voçe recebera %d notas de 100, %d notas de 50, %d notas de 20, %d notas de 10, %d notas de 5 e %d notas de 1.", nota100, nota50, nota20, nota10, nota5, nota1);
 
